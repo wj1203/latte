913a27da5dfcc3f0e0092d5f2b571ac3c4749cd3
wj1203 <541569812@qq.com>
2020-05-14 11:29:42 +0800

    jni

---
+++ common_lib/src/main/java/com/leap/common_lib/Main.java
@@ -0,0 +1,21 @@
+package com.leap.common_lib;
+
+import android.os.Handler;
+import android.os.MessageQueue;
+import android.util.LruCache;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author: leap
+ * @time: 2020/3/4
+ * @classname: Main
+ * @description:
+ */
+public class Main {
+
+    public void setMap() {
+        new HashMap<>().put(null,"");
+    }
+}
---
+++ common_lib/src/main/java/com/leap/common_lib/jni/JniTest.java
@@ -0,0 +1,14 @@
+package com.leap.common_lib.jni;
+
+/**
+ * @author: leap
+ * @time: 2020/3/3
+ * @classname: JniTest
+ * @description:
+ */
+public class JniTest {
+
+    public native String  get();
+
+
+}
